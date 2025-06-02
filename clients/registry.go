ae1ed8339e477b0618b90747fea709e4b1e2857a
Ibrahim Ahmad <ibrahim.ahmad@digitalsiber.id>
2025-06-02 20:20:10 +0700

    setup user client

---
+++ clients/registry.go
@@ -0,0 +1,27 @@
+package clients
+
+import (
+	"field-service/clients"
+	clients "field-service/clients/user"
+	config2 "field-service/config"
+	"user-service/clients/config"
+)
+
+type ClientRegistry struct {
+}
+
+type IClientRegistry interface {
+	GetUser() clients.IUserClient
+}
+
+func NewClientRegistry() IClientRegistry {
+	return &ClientRegistry{}
+}
+
+func (c *ClientRegistry) GetUser() clients.IUserClient {
+	return clients.NewUserClient(
+		config.NewClientConfig(
+			config.WithBaseURL(config2.Config.InternalService.User.Host),
+			config.WithSignatureKey(config2.Config.SignatureKey),
+		))
+}
